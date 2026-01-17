345d1321f2a232d55bf30e7301af40633faeb539
Md Mehedi Hasan <mmehedihasan.cy@gmail.com>
2026-01-18 03:10:30 +0800

    Implement automatic superuser creation
    Add automatic superuser creation on app ready.

--- FYP/backend/api/apps.py
+++ FYP/backend/api/apps.py
@@ -1,6 +1,24 @@
 from django.apps import AppConfig
+import os
 
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+        if not email or not password:
+            return
+
+        if not User.objects.filter(email=email).exists():
+            User.objects.create_superuser(
+                email=email,
+                password=password
+            )
+            print("✅ Superuser created automatically")
